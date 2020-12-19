a77c4271e8c7cb03d312ce402e44793434f7c0c7
zylai <40312846+zylai@users.noreply.github.com>
2020-12-18 18:33:49 -0800

    Update faa.py

--- faa.py
+++ faa.py
@@ -4,6 +4,13 @@ from selenium.webdriver.common.keys import Keys
 from selenium.webdriver.support.ui import Select
 import time
 
+def check():
+	try:
+		driver.find_element_by_xpath(airmen_exists)
+		return True
+	except:
+		return False
+
 driver = webdriver.Firefox()
 driver.get("https://amsrvs.registry.faa.gov/airmeninquiry/Main.aspx")
 
@@ -15,19 +22,17 @@ submit_buttom = '//*[@id="ctl00_content_ctl01_btnSearch"]'
 airmen_exists = '//*[@id="ctl00_content_ctl01_drAirmenList_ctl01_lnkbtnAirmenName"]'
 
 for year in range(1900, 2020):
-	search_year = year
-	for month in range(1, 12):
-		search_month = month
-		if ((search_month == 4) or (search_month == 6) or (search_month == 9) or (search_month == 11)):
+	for month in range(1, 13):
+		if ((month == 4) or (month == 6) or (month == 9) or (month == 11)):
 			day_limit = 30
-		elif ((search_month == 1) or (search_month == 3) or (search_month == 5) or (search_month == 7) or (search_month == 8) or (search_month == 10) or (search_month == 12)):
+		elif ((month == 1) or (month == 3) or (month == 5) or (month == 7) or (month == 8) or (month == 10) or (month == 12)):
 			day_limit = 31
 		else:
 			if (((year % 4 == 0) and (year % 100 != 0)) or (year % 400 == 0)):
 				day_limit = 29
 			else:
 				day_limit = 28
-		for day in range(1, day_limit):
+		for day in range(27, day_limit+1):
 			driver.find_element_by_xpath(lastname_field).clear();
 			driver.find_element_by_xpath(lastname_field).send_keys("lindbergh"); #using charles lindbergh to test since his DOB is publicly known
 
@@ -39,11 +44,11 @@ for year in range(1900, 2020):
 
 			driver.find_element_by_xpath(submit_buttom).click()
 
-			#time.sleep(10)
+			time.sleep(10)
 
 			assert "search criteria provided above" in driver.page_source
 
-			if (driver.find_element_by_xpath(airmen_exists)):
-				print ("Birthday found: " + str(search_month) + "/" + str(day) + "/" + str(search_year))
+			if (check()):
+				print ("Birthday found: " + str(month) + "/" + str(day) + "/" + str(year))
 				driver.close()
-				quit()
+				exit()
